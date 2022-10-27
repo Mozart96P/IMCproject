477cea380aa7f2a6a1a2e9e5ecf23451563e6ac4
Mozart96P <mozart96p@hotmail.com>
2022-10-27 16:39:45 -0300

    Concluindo comandos em JS

--- script.js
+++ script.js
@@ -2,10 +2,18 @@
 function calcularimc(){
       let valoraltura = parseFloat(document.getElementById("InformacaoAltura").value);
       let valorpeso = parseFloat(document.getElementById("InformacaoPeso").value);
+      let valorresultado = document.getElementById("Resultado");
 
       let altura = (valoraltura * valoraltura);
       let peso = valorpeso;
+      valorresultado = (peso / altura);
 
-      let resultado = (peso / altura);
-      console.log(resultado);
+      switch(conceito){
+
+      }
+      if(valorresultado >= 18.5 && valorresultado<= 24.9){
+            conseito = "Normal";
+      }
+      Resultado.innerHTML = `<p>${Math.ceil(valorresultado)}</p>`
+      
 }
